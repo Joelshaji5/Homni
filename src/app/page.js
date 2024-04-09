998bf8425260a7cc48dc1a098dd7ffca0d8216a5
joelshaji5 <joel.shaji15@gmail.com>
2024-04-09 11:08:46 +0530

    first repo test

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,13 @@ import Image from "next/image";
 import HomePage from "./Homepage/page";
 import Navbar from "./Components/Navbar/Navbar";
 import NavbarMb from "./Components/Navbar/NavbarMb";
-import Test from "./Components/Components/About/test";
 
 export default function Home() {
   return (
     <>
-    {/* <Navbar/>
+    <Navbar/>
     <NavbarMb />
-  <HomePage /> */}
-<Test />
+  <HomePage />
 
   </>
   );
