0b3f02b0e37776f7449a4f883cc056622bf6dc6b
joelshaji5 <joel.shaji15@gmail.com>
2024-05-11 20:47:17 +0530

    Google Tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+   <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PRHGG2Q7');
+            `,
+          }}
+        />
+      </Head>
 
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRHGG2Q7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        
+        {children}</body>
     </html>
   );
 }
