a94000fecffd5e5118878f0bc7583383e631371f
joelshaji5 <joel.shaji15@gmail.com>
2024-05-14 18:24:51 +0530

    google tag fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,18 +14,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
  <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBL03NQ2J4"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
 
-              gtag('config', 'G-SBL03NQ2J4');
-            `,
-          }}
-        />
+ <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBL03NQ2J4"></script>
+ <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-SBL03NQ2J4');
+    `,
+  }}
+></script>
+
+        
 
         <script
           dangerouslySetInnerHTML={{
@@ -38,6 +40,8 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+
+
       </Head>
 
 
