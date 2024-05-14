3ae4dcd3b3f084b8cbd99f986ea7e4800fc88fcc
joelshaji5 <joel.shaji15@gmail.com>
2024-05-14 18:06:10 +0530

    Google tag Fix

--- src/app/Components/Components/Footer/Footer.jsx
+++ src/app/Components/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../../Style/Footer.css";
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
@@ -27,6 +28,7 @@ function Footer() {
           <h3>Location - Pune, Navi Mumbai</h3>
           <h3>Email ID - homnident@gmail.com</h3>
           <h3>Phone number - 95037 75068</h3>
+          <h4><Link href="/TermsAndCondition">Terms & Conditions</Link></h4>
         </div>
         <div>
           <p className="FooterDivH3">Designed by - TechRift Dynamics</p>
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-   <Head>
+ <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBL03NQ2J4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-SBL03NQ2J4');
+            `,
+          }}
+        />
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -26,6 +39,7 @@ export default function RootLayout({ children }) {
           }}
         />
       </Head>
+
 
       <body className={inter.className}>
 
