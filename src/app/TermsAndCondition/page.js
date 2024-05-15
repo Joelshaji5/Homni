567ba6d270a57520c80b83bddcab94fa2462439e
joelshaji5 <joel.shaji15@gmail.com>
2024-05-15 23:41:58 +0530

    term and condition first

--- src/app/TermsAndCondition/page.js
+++ src/app/TermsAndCondition/page.js
@@ -2,9 +2,193 @@ import React from 'react'
 
 function Page() {
     return (<>
-        <div className="terms-container">
-          <h1>Terms and Conditions</h1>
-        </div>
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>
+        These terms and conditions outline the rules and regulations for the use of HOMNIDENT LLP website{' '}
+        <a href="https://www.homnident.com/">https://www.homnident.com/</a>. 
+      </p>
+      <p>
+      By accessing this website we assume you
+        accept these terms and conditions. Do not continue to use Homnident LLP WEBSITE if you do not agree to take all
+        of the terms and conditions stated on this page.
+      </p>
+      <p>
+      The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of INDIA. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
+
+      </p>
+      {/* Other sections of Terms and Conditions */}
+      <h2>Appointment Booking</h2>
+      <p>
+      By booking an appointment or by visiting our website,you consent us to perform dental treatment of checkup and consultation upon you.You also by default agree to all the terms and conditions for the following-
+       </p>
+      <ol>
+        <li>
+          <strong>Examination and X-rays</strong>
+          <p>
+          I understand that the initial visit may require radiographs in order to complete the examination, diagnosis, and treatment plan.
+          </p>
+          
+                  </li>
+                  <li>
+          <strong>Drugs, Medication and Sedation</strong>
+          <p>
+          I have been informed and understand that antibiotic, analgesics, and other medications can cause allergic reactions causing redness, swelling of tissues, pain, itching, vomiting, and/or anaphylactic shock (severe allergic reaction). They may cause drowsiness and lack of awareness and coordination, which can be increased by the use of alcohol or other drugs. I understand that and fully agree not to operate any vehicle or hazardous device for at least 12 hours or until fully recovered from the effects of the anesthetic medication and drugs that may have been given me in the office for my treatment. I understand that failure to take medications prescribed for me in the manner prescribed may offer risks of continued or aggravated infection, pain, and potential resistance to effect treatment of my condition. I understand that antibiotics can reduce the effectiveness of oral contraceptives.
+
+</p>
+        </li>
+
+
+        <li>
+          <strong>Changes in Treatment Plan</strong>
+          <p>
+          I understand that during treatment, it may be necessary to change or add procedures because of conditions found while working on teeth that were not discovered during examination, the most common being root canal therapy following routine restorative procedures. I give my permission to the Dentist to make any or all changes and additions as necessary.
+
+</p>
+        </li>
+
+
+        <li>
+          <strong>. Temporomandibular Joint Dysfunctions (TMJ)</strong>
+          <p>
+          I understand that symptoms of popping, clicking, locking and pain can intensify or develop in the joint of the lower (near the ear) subsequent to routine dental treatment wherein the mouth is held in the open position. However, symptoms of TMJ associated with dental treatment are usually transitory in nature and well tolerated by most patients. I understand that should the need for treatment arise, then I will be referred to a specialist for treatment, and the cost of which is my responsibility.
+
+</p>
+        </li>
+
+        <li>
+          <strong>Fillings</strong>
+          <p>
+          I understand that care must be exercised in chewing on filling during the first 24 hours to avoid breakage, and tooth sensitivity is common after-effect of a newly placed filling.
+</p>
+        </li>
+
+        <li>
+          <strong>Removal of Teeth (Extraction)</strong>
+          <p>
+   Alternative to removal has been explained to me (root canal therapy, crowns, periodontal surgery, etc.) and I authorize the Dentist to remove the following teeth and any others necessary for the reasons in paragraph #3. I understand removing teeth does not always remove all infection if present and it may be necessary to have further treatment. I understand the risks involved is having teeth removed, some of which are pain, swelling, and spread of infection, dry socket, loss of feeling in my teeth, lips, tongue, and surrounding tissue (parathesia) that can last for an indefinite period of time or fractured jaw. I understand I may need further treatment by a specialist or even hospitalization if complications arise during or following treatment, the cost of which is my responsibility.
+</p>
+        </li>
+
+        <li>
+          <strong>Crowns, Bridges, Veneers and Bonding</strong>
+          <p>
+          I understand that sometimes it is not possible to match the color of natural teeth exactly with artificial teeth. I further understand that I may be wearing temporary crowns, which may come off easily and that I must be careful to ensure that they are kept on until the permanent crowns are delivered. I realized that the final opportunity to make changes in my new crowns, bridge or cap (including shape, fit, size, placement, and color) will be done before cementation. It has been explained to me that, in very few cases, cosmetic procedures may result in the need for future root canal treatment, which cannot always be predicted or anticipated. I understand that cosmetic procedures may affect tooth surfaces and may require modification of daily cleaning procedures.</p>
+        </li>
+
+        <li>
+          <strong>Dentures – Complete or Partial</strong>
+          <p>
+          realize that full or partial dentures are artificial, constructed of plastic, metal and or porcelain. The problems of wearing those appliances have been explained to me including looseness, soreness, and possible breakage. I realize the final opportunity to make changes in my new denture (including shape, fit, size, placement, and color) will be “teeth in wax” try-in visit. I understand that most dentures require relining approximately three to twelve months after initial placement. The cost for this procedure is not the initial denture fee.</p>
+        </li>
+
+        <li>
+          <strong>Endodontic Treatment (Root Canal)</strong>
+          <p>
+          I realize there is no guarantee that root canal treatment will save my tooth and those complications can occur from the treatment and that occasionally metal objects are cemented in the tooth, or extend through the root, which does not necessarily affect the success of the treatment. I understand that occasionally additional surgical procedures may be necessary following root canal treatment (apicoectomy).    
+        </p>
+        </li>
+        <li>
+          <strong>Periodontal Treatment</strong>
+          <p>
+          I understand that I have a serious condition causing gum inflammation and/or bone loss and that it can lead to the loss of my teeth. Alternative treatment plans have been explained to me, including non-surgical cleaning, gum surgery and/or extractions. I understand the success of a treatment depends in part on my efforts to brush and floss daily, receive regular cleaning as directed, following a healthy diet, avoid tobacco products and follow other recommendations.
+     </p>
+        </li>
+        {/* Other appointment terms */}
+      </ol>
+      {/* Cookie section */}
+      <h2>Cookies</h2>
+      <p>
+      We employ the use of cookies. By accessing Homnident LLP, you agreed to use cookies in agreement with the Homnident LLP's Privacy Policy.
+
+      </p>
+      <p>
+      Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
+      </p>
+      {/* License section */}
+      <h2>License</h2>
+      <p>
+      Unless otherwise stated, Homnident LLP and/or its licensors own the intellectual property rights for all material on Homnident LLP. All intellectual property rights are reserved. You may access this from Homnident LLP for your own personal use subjected to restrictions set in these terms and conditions..
+      </p>
+      <p>You must not:
+</p>
+<ol>
+    <li>
+    Republish material from Homnident LLP
+
+    </li>
+
+    <li>
+    Sell, rent or sub-license material from Homnident LLP
+
+    </li>
+
+    <li>
+    Reproduce, duplicate or copy material from Homnident LLP
+
+    </li>
+
+    <li>
+    Redistribute content from Homnident LLP
+
+    </li>
+</ol>
+<p>
+This Agreement shall begin on the date hereof.
+
+</p>
+<p>
+    
+Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Homnident LLP does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Homnident LLP,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Homnident LLP shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
+
+</p>
+
+<p>
+Homnident LLP reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
+</p>
+<p>
+You warrant and represent that:
+ 
+</p>
+
+<ol>
+    <li>
+    You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;
+
+    </li>
+    <li>
+    The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;
+
+    </li>
+    <li>
+    The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy
+
+    </li>
+    <li>
+    The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.
+
+    </li>
+</ol>
+<p>
+You hereby grant Homnident LLP a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
+
+</p>
+
+      {/* Hyperlinking section */}
+      <h2>Hyperlinking to our Content</h2>
+      <p>
+      The following organizations may link to our Website without prior written approval:
+
+      </p>
+      {/* Other sections */}
+      {/* Disclaimer section */}
+      <h2>Disclaimer</h2>
+      <p>
+        To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions
+        relating to our website and the use of this website.
+      </p>
+    </div>
+
         </>
       );
     }
