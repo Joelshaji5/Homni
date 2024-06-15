1ba21e3b8412557cf77380fbe1ebffd57a62e1d5
joelshaji5 <joel.shaji15@gmail.com>
2024-06-15 18:20:40 +0530

    sync fix 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Script from "next/script";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,57 +14,62 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
- <Head>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
 
- <script 
-      type="text/javascript"
-      src="https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js"
-      id="aisensy-wa-widget"
-      widget-id="Un6wwC"
-    >
-    </script>
+      <body className={inter.className}>
+        <Script
+          src="https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js"
+          id="aisensy-wa-widget"
+          widget-id="Un6wwC"
+          strategy="afterInteractive"
+        />
 
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SBL03NQ2J4"
+        />
 
- {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBL03NQ2J4"></script> */}
- <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-SBL03NQ2J4');
-    `,
-  }}
-></script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SBL03NQ2J4');
+            `,
+          }}
+        />
 
-        
-
-        <script
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l;j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PRHGG2Q7');
             `,
           }}
         />
 
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PRHGG2Q7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
 
-      </Head>
-
-
-      <body className={inter.className}>
-
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRHGG2Q7"
-height="0" width="0"     style={{ display: 'none', visibility: 'hidden' }}
->
-  
-  
-  </iframe></noscript>
-        
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
